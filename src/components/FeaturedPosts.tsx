18e55f40bbf51d8a1ed9e063af6fb877218d6364
suzz-in <o_o_su_jin_@naver.com>
2023-04-03 14:20:44 +0900

    feat: carousel 사용

--- src/components/FeaturedPosts.tsx
+++ src/components/FeaturedPosts.tsx
@@ -4,7 +4,7 @@ import PostGrid from "./PostGrid";
 export default async function FeaturedPosts() {
 
     const posts = await getFeaturedPosts()
-return <section>
+return <section className="my-4">
     <h2 className="text-2xl font-bold my-2">Featured Posts</h2>
     <PostGrid posts={posts}/>
 </section>
---
+++ src/components/MultiCarousel.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4,
+    slidesToSlide: 3 // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 3,
+    slidesToSlide: 2 // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 2,
+    slidesToSlide: 1 // optional, default to 1.
+  }
+};
+
+type Props = {
+    children: React.ReactNode
+}
+
+export default function MultiCarousel({children}:Props) {
+
+return <Carousel infinite autoPlay responsive={responsive} itemClass="m-2">
+    {children}
+</Carousel>
+}
