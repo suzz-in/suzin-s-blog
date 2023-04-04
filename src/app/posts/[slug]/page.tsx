4ae7d353831dee8452d73a50af4d4bdaabaf094f
suzz-in <o_o_su_jin_@naver.com>
2023-04-04 16:13:13 +0900

    feat: 이전, 다음 게시글 이동

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,7 +1,7 @@
-import MarkdownViewer from "@/components/MarkdownViewer";
-import { getPostData } from "@/service/posts";
+import AdjacentPostCard from "@/components/AdjacentPostCard";
+import PostContent from "@/components/PostContent";
+import { getAllPosts, getPostData } from "@/service/posts";
 import Image from "next/image";
-import {AiTwotoneCalendar} from "react-icons/ai"
 
 type Props = {
     params: {
@@ -10,27 +10,22 @@ type Props = {
 }
 
 export default async function page({params:{slug}}: Props) {
-    const post = await getPostData(slug);
-
+    const post  = await getPostData(slug);
+    const {path, title, next, prev} = post;
 
 return (
     <article className="rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4 ">
         <Image 
         className="w-full h-1/5 max-h-[500px]"
-        src={`/images/posts/${post.path}.png`}
-        alt={post.title}
+        src={`/images/posts/${path}.png`}
+        alt={title}
         width={760}
         height={420}
         />
-        <section className="flex flex-col p-4">
-            <div className="flex items-center self-end text-sky-600">
-                <AiTwotoneCalendar />
-                <p className="font-semibold ml-2">{post.date.toString()}</p>
-            </div>
-        <h1 className="text-4xl font-bold">{post.title}</h1>
-        <p className="text-xl font-bold">{post.description}</p>
-        <div className="w-44 border-2 border-sky-600 mt-4 mb-8"/>
-        <MarkdownViewer content={post.content}/>
+        <PostContent post={post}/>
+        <section className="flex shadow-md">
+            {prev && <AdjacentPostCard post={prev} type="prev"/>}
+            {next && <AdjacentPostCard post={next} type="next"/>}
         </section>
     </article>
 )
--- src/service/posts.ts
+++ src/service/posts.ts
@@ -9,7 +9,7 @@ export type Post = {
     path: string;
     featured: boolean;
 }
-export type PostData = Post & {content: string};
+export type PostData = Post & {content: string; next: Post | null; prev: Post | null;};
 
 export async function getFeaturedPosts(): Promise<Post[]> {
     return getAllPosts()
@@ -24,13 +24,18 @@ export async function getNonFeaturedPosts(): Promise<Post[]> {
 
 export async function getPostData(fileName:string): Promise<PostData> {
     const filePath = path.join(process.cwd(), 'data','posts', `${fileName}.md`);
-    const metaData = await getAllPosts()
-    .then((post)=> post.find((post)=> post.path === fileName))
-    if(!metaData) throw new Error(`${fileName}에 해당하는 포스트를 찾을 수 없음`)
+    const posts = await getAllPosts();
+    const post = posts.find((post)=> post.path === fileName)
+    
+    if(!post) throw new Error(`${fileName}에 해당하는 포스트를 찾을 수 없음`)
+
+    const index = posts.indexOf(post);
+    const next = index > 0 ? posts[index-1] : null;
+    const prev = index < posts.length ? posts[index + 1 ] : null;
 
     const content = await readFile(filePath, 'utf-8');
 
-    return {...metaData, content}
+    return {...post, content, next, prev}
 }
 
 
